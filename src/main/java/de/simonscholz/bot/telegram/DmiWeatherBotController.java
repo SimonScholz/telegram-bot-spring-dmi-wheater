473257f44b40e3a9b18b0063e7fa8237c00db372
Simon Scholz <simon.scholz@vogella.com>
2018-04-17 15:39:05 +0200

    Added general request mapping for the controller to /bot
    Change-Id: I789229f8c1c2e1538c8d4560714ed8823f0756f8
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- src/main/java/de/simonscholz/bot/telegram/DmiWeatherBotController.java
+++ src/main/java/de/simonscholz/bot/telegram/DmiWeatherBotController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ import io.micrometer.core.instrument.MeterRegistry;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/bot")
 class DmiWeatherBotController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(DmiWeatherBotController.class);
